5f0fb7b95a823c24739bd22298cb4130d8d630c4
Millex <millelinux@gmail.com>
2016-10-08 18:19:19 +0200

    modifiche anti_debug

--- Skanderbeg/Skanderbeg_sri_mir_main.c
+++ Skanderbeg/Skanderbeg_sri_mir_main.c
@@ -5,15 +5,32 @@
 
 int main()
 {
-    
-	//controllo debug prima del main 
+    //
+	//controllo debug prima del main junk e no exit (forse "goto" spam?)
+	//
+
+
 	if (IsDebuggerPresent())
 	{
 		printf("madafaka");
 		exit;
 	}
-	
-	
+    
+	//
+	//VEDERE SU IDA  
+	//
+	char trol = 0;
+	__asm {
+		mov eax, fs:[30h]
+		mov al, [eax + 2h]
+		mov trol, al
+	}
+
+	if (trol)
+	{
+		printf("swsss");
+		exit;
+	}
 	
 	
 	
